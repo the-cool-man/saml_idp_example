e71860846cbb7bdf43c44f21dc6c7a651d7c95e5
“Tohid <kovadiya.tohid@gmail.com>
2018-08-20 20:19:19 +0100

    simple php IDP solution

---
+++ src/Utility/IdpProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+class IdpProvider {
+
+  // Defining some trusted Service Providers.
+  private $trusted_sps = [
+    'IAMShowcase' => 'https://sptest.iamshowcase.com/acs'
+  ];
+
+  /**
+   * Retrieves the Assertion Consumer Service.
+   *
+   * @param string
+   *   The Service Provider Entity Id
+   * @return
+   *   The Assertion Consumer Service Url.
+   */
+  public function getServiceProviderAcs($entityId){
+    return $this->trusted_sps[$entityId];
+  }
+
+  /**
+   * Returning a dummy IdP identifier.
+   *
+   * @return string
+   */
+  public function getIdPId(){
+    return "https://app.mogl.online";
+  }
+
+  /**
+   * Retrieves the certificate from the IdP.
+   *
+   * @return \LightSaml\Credential\X509Certificate
+   */
+  public function getCertificate(){
+    return \LightSaml\Credential\X509Certificate::fromFile('cert/saml_test_certificate.crt');
+  }
+
+  /**
+   * Retrieves the private key from the Idp.
+   *
+   * @return \RobRichards\XMLSecLibs\XMLSecurityKey
+   */
+  public function getPrivateKey(){
+    return \LightSaml\Credential\KeyHelper::createPrivateKey('cert/saml_test_certificate.key', '', true);
+  }
+
+  /**
+   * Returns a dummy user email.
+   *
+   * @return string
+   */
+  public function getUserEmail(){
+
+    return "tohid@outliant.com";
+  }
+
+}
